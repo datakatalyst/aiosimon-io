c612053be5f36e63514e72f0f77c150fed8f423c
Datakatalyst <2864836+datakatalyst@users.noreply.github.com>
2025-04-04 14:26:55 +0200

    chore: Fix black in conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -5,15 +5,16 @@
 
 import os
 import sys
+
 sys.path.insert(0, os.path.abspath("../../"))
 
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project = 'aiosimon-io'
-copyright = '2025, Datakatalyst'
-author = 'Datakatalyst'
-release = '0.1.0'
+project = "aiosimon-io"
+copyright = "2025, Datakatalyst"
+author = "Datakatalyst"
+release = "0.1.0"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
@@ -34,4 +35,4 @@ exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
 html_theme = "sphinx_rtd_theme"
-html_static_path = ['_static']
+html_static_path = ["_static"]
